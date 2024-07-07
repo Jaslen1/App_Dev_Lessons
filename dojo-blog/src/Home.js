f2d0fab322e5a0c1b1d93828f9bb33d3b727bee7
Jaslen1 <jleenokaur@gmail.com>
2024-07-07 15:16:09 -0700

    finished lesson 4

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,23 +1,19 @@
-import {useState} from 'react';
+import { useState } from "react";
+import BlogList from "./Bloglist"
 
 const Home = () => {
- //let name = 'mario' ;
- const [name, setName] = useState('mario')
- const[age, setAge] = useState(25);
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-
-  }
-
-    return (
-      <div className="home">
-        <h2>Homepage</h2>
-        <p>{ name } is {age} years old</p>
-        <button onClick= {handleClick}>Click me</button>
-      </div>
-    );
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+    </div>
+  );
 }
-
+ 
 export default Home;
