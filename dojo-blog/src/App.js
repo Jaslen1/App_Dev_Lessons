3b8aaef668d5174548a49eee9338c6aad8b9077d
Jaslen1 <jleenokaur@gmail.com>
2024-07-07 13:04:32 -0700

    lesson 2

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,27 +1,15 @@
-import './App.css';
+import Navbar from './navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new Blog';
-  const likes = 50;
-  //const person = { name: 'yoshi',age: 30 };
-  const link= "http://www.google.com";
-
   return (
-    <div className= "App">
+<div className="App">
+      <Navbar />
       <div className= "content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas"}</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-  
-        <a href={link}>Google Site</a>
-       </div>
+        <Home />
       </div>
+    </div>
   );
 }
+
 export default App;
