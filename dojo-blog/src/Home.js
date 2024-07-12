e867b08afdfbe3b120eb9bc27dfa0780443fb699
Jaslen1 <jleenokaur@gmail.com>
2024-07-12 15:44:43 -0700

    lesson 19 before making a custom hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,27 +1,37 @@
-import { useState, useEffect } from "react";
-import BlogList from "./Bloglist"
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-    fetch('http://localhost:8000/blogs')
+      fetch('http://localhost:8000/blogs')
       .then(res => {
+        if (!res.ok) { 
+          throw Error('could not fetch the data for that resource');
+        } 
         return res.json();
       })
       .then(data => {
-        setBlogs(data)
-        setIsPending(false)
-      });
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
     }, 1000);
-  }, []);
+  }, [])
 
   return (
     <div className="home">
-      {isPending && <div>Loading....</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title= "All Blogs!" /> }
     </div>
   );
 }
