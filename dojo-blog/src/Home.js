2a79ef38c018470e5b44ad870733f0019781a771
Jaslen1 <jleenokaur@gmail.com>
2024-07-12 12:04:18 -0700

    lesson 16 before Fetching Data with useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -6,9 +6,7 @@ const Home = () => {
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const [name, setName] = useState('mario');
+  ]);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !==id);
@@ -17,16 +15,13 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(name);
-  },[name]);
+  }, []);
 
 
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-      <button onClick={()=> setName('luigi')}>Change name</button>
-      <p>{ name }</p>
     </div>
   );
 }
